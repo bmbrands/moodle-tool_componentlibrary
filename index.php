3f907da84c6cc3f5389ccd6482d096a1c50a66b9
Gareth Barnard <1058419+gjb2048@users.noreply.github.com>
2020-01-13 16:57:05 +0000

    Improve and make index.php work!

--- index.php
+++ index.php
@@ -3,34 +3,25 @@
 require_once(__DIR__ . '/../../../config.php');
 
 $PAGE->set_pagelayout('admin');
-$url = new moodle_url('/admin/tool/componentlibrary/index.php');
-$PAGE->set_url($url);
+$thispageurl = new moodle_url('/admin/tool/componentlibrary/index.php');
+$PAGE->set_url($thispageurl, $thispageurl->params());
 $PAGE->set_context(context_system::instance());
-$PAGE->set_heading('Component Library test');
-$PAGE->set_title('Component Library test');
+$title = 'Component Library test';
+$PAGE->set_heading($title);
+$PAGE->set_title($title);
+$PAGE->set_docs_path('');
 
+// No edit.
+$USER->editing = $edit = 0;
 
-$urls = $PAGE->theme->css_urls($PAGE);
-if (count($urls) === 1) {
-    $cssurl =  $urls[0]->out();
-}
+// Add navbar.
+$PAGE->navbar->ignore_active();
+$PAGE->navbar->add($PAGE->title, $thispageurl);
 
-$renderer = new core_renderer($PAGE, 'frontpage');
-$headhtml = $renderer->standard_head_html();
-$jsheader = $PAGE->requires->get_head_code($PAGE, $renderer);
-$jsurl = $PAGE->theme->javascript_url(false);
-$jsfooter = $PAGE->requires->get_end_code();
+echo $OUTPUT->header();
+echo $OUTPUT->box_start();
 
-echo '
-<head>
-' . $headhtml  . '
-<link rel="stylesheet" type="text/css" href="' . $cssurl . '">
-</head>
-<body>
-
-' . $jsurl . '
-
-<div class="container my-4">
+echo '<div class="container my-4">
     <button type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="top" title="Tooltip on top">
       Tooltip on top
     </button>
@@ -43,8 +34,8 @@ echo '
     <button type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="left" title="Tooltip on left">
       Tooltip on left
     </button>
-</div>
-' . $jsfooter . '
-</body>
-';
+</div>';
 
+echo $OUTPUT->box_end();
+
+echo $OUTPUT->footer();
